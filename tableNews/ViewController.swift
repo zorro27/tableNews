152130fe2a38c86bf6b6f801b324681240a87ba8
zorro27 <73430357+zorro27@users.noreply.github.com>
2022-02-14 21:23:32 +0300

    Initial Commit

---
+++ tableNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableNews
+//
+//  Created by Роман Зобнин on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
